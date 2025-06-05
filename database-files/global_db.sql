45b14b62887fbc358bba25fe4c024cbb844817f6
carter-vargas <105988277+carter-vargas@users.noreply.github.com>
2025-06-05 16:42:26 +0200

    added economist stuff

--- database-files/global_db.sql
+++ database-files/global_db.sql
@@ -3,8 +3,100 @@ CREATE DATABASE IF NOT EXISTS global_database;
 
 USE global_database;
 
+CREATE TABLE IF NOT EXISTS UserTypes (
+    type_id INT AUTO_INCREMENT PRIMARY KEY,
+    type_name VARCHAR(25)
+);
+
+CREATE TABLE IF NOT EXISTS Interest (
+    interest_id INT AUTO_INCREMENT PRIMARY KEY,
+    interest_name VARCHAR(25)
+);
+
 CREATE TABLE IF NOT EXISTS Users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    type_id INT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    interest_id INT,
+    FOREIGN KEY (type_id) REFERENCES UserTypes(type_id),
+    FOREIGN KEY (interest_id) REFERENCES Interest(interest_id)
+);
+
+CREATE TABLE IF NOT EXISTS SavedPolicy (
+    saved_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    frdr INT,
+    fbss INT,
+    tsh INT,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS SP500 (
+    sp_id INT AUTO_INCREMENT PRIMARY KEY,
+    date DATE,
+    close DECIMAL(10, 5)
+);
+
+CREATE TABLE IF NOT EXISTS GDPUS (
+    gdpus_id INT AUTO_INCREMENT PRIMARY KEY,
+    date DATE,
+    close DECIMAL(10, 5)
+);
+
+CREATE TABLE IF NOT EXISTS GDPWORLD (
+    gdpworld_id INT AUTO_INCREMENT PRIMARY KEY,
+    date DATE,
+    close DECIMAL(10, 5)
+);
+
+CREATE TABLE IF NOT EXISTS URTH (
+    urth_id INT AUTO_INCREMENT PRIMARY KEY,
+    date DATE,
+    close DECIMAL(10, 5)
+);
+
+CREATE TABLE IF NOT EXISTS ProposedPolicy (
+    proposed_policy_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    scope VARCHAR(20),
+    enforcement VARCHAR(20),
+    duration VARCHAR(20),
+    country_id INT,
+    FOREIGN KEY (country_id) REFERENCES Countries(country_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE HistoricalPolicy (
+    historical_policy_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    country_id INT,
+    policy_name VARCHAR(100),
+    description VARCHAR(255),
+    category VARCHAR(100),
+    date DATE,
+    FOREIGN KEY (country_id) REFERENCES Countries(country_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS Politicians (
+    politician_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    contact_info VARCHAR(250),
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS Conversations (
+    conversation_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    politician_id INT,
+    saved_id INT,
+    content TEXT,
+    title VARCHAR(50)
+    -- FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    -- FOREIGN KEY (saved_id) REFERENCES SavedPolicy(saved_id),
+    -- FOREIGN KEY (politician_id) REFERENCES Politician(politician_id)
 );
 
 CREATE TABLE IF NOT EXISTS Policies (
@@ -27,16 +119,6 @@ CREATE TABLE IF NOT EXISTS Favorite_Policies (
     FOREIGN KEY (policy_id) REFERENCES Policies(policy_id),
     FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
-
-CREATE TABLE IF NOT EXISTS Politicians (
-    polyid INT PRIMARY KEY,
-    age INT,
-    email VARCHAR(50),
-    office_phone VARCHAR(50)
-);
-
-INSERT INTO Users (user_id)
-    VALUES (1);
 
 INSERT INTO Policies (policy_id, year_enacted, politician, topic, country, pol_scope, duration, intensity, advocacy_method, pol_description) VALUES
 (1, 2007, 'Damon Krill', 'Public deficit', 'China', 'State', 'Trial', 'Light Enforcement', 'Meme Campaign', 'This policy addresses key challenges with an innovative approach. Its implementation is expected to impact multiple sectors positively.'),
@@ -539,101 +621,6 @@ INSERT INTO Policies (policy_id, year_enacted, politician, topic, country, pol_s
 (498, 2016, 'Bryce Linwood', 'Tariffs', 'EU', 'Neighborhood', 'Permanent', 'Strict Enforcement', 'Newsletter', 'Targets economic stability through strategic planning and enforcement. Collaboration across agencies is central to success.'),
 (499, 2023, 'Hank Marx', 'Govt spending', 'China', 'International', 'Permanent', 'Minimal', 'Petition', 'Designed to improve efficiency and sustainability within the region. Ongoing evaluation will determine future adjustments.'),
 (500, 2000, 'Hank Marx', 'Interest rates', 'USA', 'National', 'Long-Term', 'Moderate Enforcement', 'Direct Email to Representative', 'This initiative enhances transparency and accountability. Public involvement has increased since inception.');
-CREATE TABLE IF NOT EXISTS UserTypes (
-    type_id INT AUTO_INCREMENT PRIMARY KEY,
-    type_name VARCHAR(25)
-);
-
-CREATE TABLE IF NOT EXISTS Interest (
-    interest_id INT AUTO_INCREMENT PRIMARY KEY,
-    interest_name VARCHAR(25)
-);
-
-CREATE TABLE IF NOT EXISTS Users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    type_id INT,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    interest_id INT,
-    FOREIGN KEY (type_id) REFERENCES UserTypes(type_id),
-    FOREIGN KEY (interest_id) REFERENCES Interest(interest_id)
-);
-
-CREATE TABLE IF NOT EXISTS SavedPolicy (
-    saved_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    frdr INT,
-    fbss INT,
-    tsh INT,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
-);
-
-CREATE TABLE IF NOT EXISTS SP500 (
-    sp_id INT AUTO_INCREMENT PRIMARY KEY,
-    date DATE,
-    close DECIMAL(10, 5)
-);
-
-CREATE TABLE IF NOT EXISTS GDPUS (
-    gdpus_id INT AUTO_INCREMENT PRIMARY KEY,
-    date DATE,
-    close DECIMAL(10, 5)
-);
-
-CREATE TABLE IF NOT EXISTS GDPWORLD (
-    gdpworld_id INT AUTO_INCREMENT PRIMARY KEY,
-    date DATE,
-    close DECIMAL(10, 5)
-);
-
-CREATE TABLE IF NOT EXISTS URTH (
-    urth_id INT AUTO_INCREMENT PRIMARY KEY,
-    date DATE,
-    close DECIMAL(10, 5)
-);
-
-CREATE TABLE IF NOT EXISTS ProposedPolicy (
-    proposed_policy_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    scope VARCHAR(20),
-    enforcement VARCHAR(20),
-    duration VARCHAR(20),
-    country_id INT,
-    FOREIGN KEY (country_id) REFERENCES Countries(country_id),
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
-);
-
-CREATE TABLE HistoricalPolicy (
-    historical_policy_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    country_id INT,
-    policy_name VARCHAR(100),
-    description VARCHAR(255),
-    category VARCHAR(100),
-    date DATE,
-    FOREIGN KEY (country_id) REFERENCES Countries(country_id),
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
-);
-
-CREATE TABLE IF NOT EXISTS Politicians (
-    politician_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50),
-    contact_info VARCHAR(250),
-    user_id INT,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
-);
-
-CREATE TABLE IF NOT EXISTS Conversations (
-    conversation_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    politician_id INT,
-    saved_id INT,
-    content TEXT,
-    title VARCHAR(50)
-    -- FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    -- FOREIGN KEY (saved_id) REFERENCES SavedPolicy(saved_id),
-    -- FOREIGN KEY (politician_id) REFERENCES Politician(politician_id)
-);
 
 INSERT INTO UserTypes (type_name)
 VALUES
